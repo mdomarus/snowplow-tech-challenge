98a9674765a7dd146d7397203a6e2ad05d18431e
Michał Domarus <mdomarus@gmail.com>
2025-01-23 16:33:05 +0000

    Add vitest

---
+++ src/utils/cn.test.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect } from 'vitest';
+import { cn } from './cn';
+
+describe('cn', () => {
+  it('should not crash without arguments', () => {
+    const result = cn();
+
+    expect(result).toEqual('');
+  });
+
+  it('should not crash with empty array', () => {
+    const inputs: string[] = [];
+
+    const result = cn(...inputs);
+
+    expect(result).toEqual('');
+  });
+
+  it('should return a string', () => {
+    const inputs = ['foo', 'bar'];
+
+    const result = cn(...inputs);
+
+    expect(result).toEqual('foo bar');
+  });
+
+  it('should remove tailwind overwrites', () => {
+    const inputs = ['p-2 border-1 text-bold', 'border-2'];
+
+    const result = cn(...inputs);
+
+    expect(result).toEqual('p-2 text-bold border-2');
+  });
+});
