98ef3dba4875aac0ff94be365241435e82c7e5bf
Michał Domarus <mdomarus@gmail.com>
2025-01-23 12:13:36 +0000

    Add header

--- src/components/layout/root.tsx
+++ src/components/layout/root.tsx
@@ -4,7 +4,8 @@ import { ReactNode } from 'react';
 const Root = (): ReactNode => (
   <div className="container mx-auto px-4">
     <header className="border-b py-4">
-      <Link to="/" className="text-xl text-teal-800">
+      <h1 className="mb-2 text-3xl text-teal-800">Snowplow Tech Challenge</h1>
+      <Link to="/" className="text-xl">
         Home
       </Link>
     </header>
