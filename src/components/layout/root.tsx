fda99b839233d9fe7d00cefc80af21ffb9848b2a
Michał Domarus <mdomarus@gmail.com>
2025-01-23 12:32:55 +0000

    Update nav link

--- src/components/layout/root.tsx
+++ src/components/layout/root.tsx
@@ -5,7 +5,10 @@ const Root = (): ReactNode => (
   <div className="container mx-auto px-4">
     <header className="border-b py-4">
       <h1 className="mb-4 text-3xl text-teal-800">Snowplow Tech Challenge</h1>
-      <Link to="/" className="text-lg">
+      <Link
+        to="/"
+        className="text-lg underline underline-offset-4 transition-all hover:underline-offset-2"
+      >
         Home
       </Link>
     </header>
